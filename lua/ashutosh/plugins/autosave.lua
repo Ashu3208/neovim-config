e48542ef673e1bcb5590d9b7b71d152c0d29e78c
Ashutosh Agrawal <92543518+Ashu3208@users.noreply.github.com>
2025-04-09 12:48:49 +0530

    Add files via upload

--- lua/ashutosh/plugins/autosave.lua
+++ lua/ashutosh/plugins/autosave.lua
@@ -1,22 +1,37 @@
 return {
-	"pocco81/auto-save.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		debounce_delay = 5000,
+	{
+		"okuuva/auto-save.nvim",
+		cmd = "ASToggle",
+		event = { "InsertLeave", "TextChanged" },
+		config = function()
+			require("auto-save").setup({
+				enabled = true,
+				execution_message = {
+					enabled = true,
+					message = function()
+						return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+					end,
+					dim = 0.18,
+					cleaning_interval = 1250,
+				},
+				trigger_events = {
+					immediate_save = { "BufLeave", "FocusLost" },
+					defer_save = { "InsertLeave", "TextChanged" },
+					cancel_defered_save = { "InsertEnter" },
+				},
+				condition = nil,
+				write_all_buffers = false,
+				noautocmd = false,
+				lockmarks = false,
+				debounce_delay = 1000,
+				debug = false,
+			})
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>ns",
+				":ASToggle<CR>",
+				{ noremap = true, silent = true, desc = "Toggle Auto Save (⌨️ <leader>ns)" }
+			)
+		end,
 	},
-	config = function()
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>ns",
-			":ASToggle<CR>",
-			{ noremap = true, silent = true, desc = "Toggle Auto Save" }
-		)
-	end,
 }
